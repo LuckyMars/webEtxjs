4c33f60914d8b9ddaa92f8d6d0d987e867367cb6
cooper <enen7373@gmail.com>
2020-12-09 11:40:10 +0800

    gird html page add

--- grid.panel/grid.js
+++ grid.panel/grid.js
@@ -1,7 +1,5 @@
  var communityHandleGrid = Ext.create("Ext.grid.Panel",{
         scrollable: 'y',
-        id: 'enclosureGrid',
-        store: null,
         split: false,
         region: 'center',
         width: '98.5%',
@@ -16,7 +14,7 @@
             selType: 'checkboxmodel',
             mode: 'Multi'
         },
-        store: listStore,
+
         region: 'center',
         viewConfig :{enableTextSelection:true},
         columns: [
@@ -24,53 +22,32 @@
                 text: "账号",
                 align: 'center',
                 flex: 1,
-                dataIndex: "account"
             },
             {
                 text: "任务名称",
                 align: 'center',
                 flex: 1,
-                dataIndex: "taskName"
             },
             {
                 text: "积分名称",
                 align: 'center',
                 flex: 1,
-                dataIndex: "pointName"
             },
             {
                 text: "开始数值",
                 align: 'center',
                 flex: 1,
-                dataIndex: "startValue"
             },
             {
                 text: "结束数值",
                 align: 'center',
                 flex: 1,
-                dataIndex: "endValue"
             },
-            {
-                text: "开始日期",
-                align: 'center',
-                flex: 1,
-                dataIndex: "startTime",
-                renderer:Ext.util.Format.dateRenderer('Y-m-d h:m:s')
-
-            },
-
-            {
-                text: "结束日期",
-                align: 'center',
-                flex: 1,
-                dataIndex: "endTime",
-                renderer:Ext.util.Format.dateRenderer('Y-m-d h:m:s')
-
-            },
+           
         ]
        
     });
-显示列表
+
 Ext.create('Ext.Viewport', {
         items: [
             {
