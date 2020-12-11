dc2e0492c611711d7b0fd3e1cbaff0dba74d37f1
cooper <enen7373@gmail.com>
2020-12-11 16:49:00 +0800

    update grid model

--- grid.panel/grid.js
+++ grid.panel/grid.js
@@ -1,60 +1,84 @@
- var communityHandleGrid = Ext.create("Ext.grid.Panel",{
-        scrollable: 'y',
-        split: false,
-        region: 'center',
-        width: '98.5%',
-        height : 950,
-        autoHeight:true,
-        margin: '20 0 20 0',
-        autoScroll: true,
-        frame: true,
-        columnLines: true,
-        multiSelect: false,
-        selModel: {
-            selType: 'checkboxmodel',
-            mode: 'Multi'
-        },
+Ext.onReady(function () {
 
-        region: 'center',
-        viewConfig :{enableTextSelection:true},
-        columns: [
-            {
-                text: "账号",
-                align: 'center',
-                flex: 1,
-            },
-            {
-                text: "任务名称",
-                align: 'center',
-                flex: 1,
-            },
-            {
-                text: "积分名称",
-                align: 'center',
-                flex: 1,
-            },
-            {
-                text: "开始数值",
-                align: 'center',
-                flex: 1,
-            },
-            {
-                text: "结束数值",
-                align: 'center',
-                flex: 1,
-            },
-           
-        ]
-       
-    });
+
+//1.定义Model
+Ext.define("MyApp.model.User", {
+    extend: "Ext.data.Model",
+    fields: [
+        { name: 'name', type: 'string' },
+        { name: 'age', type: 'int' },
+        { name: 'phone', type: 'string' }
+    ]
+});
+
+//2.创建store
+var provice = [//本地数据
+    ['张三', "20", "123456456"],
+    ['李四', "21", "141414"],
+    ['王五', "22", "141414141"],
+    ['赵柳', "23", "14141414"]
+];
+var store = Ext.create("Ext.data.Store", {
+  model: "MyApp.model.User",
+  autoLoad: true,
+  pageSize: 5,
+  proxy: {
+     
+      type: "memory",//获取本地数据
+      data: provice,
+      reader: "array"
+  }
+});
+
+//3.创建grid
+var grid = Ext.create("Ext.grid.Panel", {
+    xtype: "grid",
+    store: store,
+    width: 500,
+    height: 200,
+    margin: 30,
+    columnLines: true,
+    renderTo: Ext.getBody(),
+    selModel: {
+        injectCheckbox: 0,
+        mode: "SIMPLE",     //"SINGLE"/"SIMPLE"/"MULTI"
+        checkOnly: true     //只能通过checkbox选择
+    },
+    selType: "checkboxmodel",
+    columns: [
+        { text: '姓名', dataIndex: 'name' },
+        {
+            text: '年龄', dataIndex: 'age', xtype: 'numbercolumn', format: '0',
+            editor: {
+                xtype: "numberfield",
+                decimalPrecision: 0,
+                selectOnFocus: true
+            }
+        },
+        { text: '电话', dataIndex: 'phone', editor: "textfield" }
+    ],
+    plugins: [
+        Ext.create('Ext.grid.plugin.CellEditing', {
+            clicksToEdit: 1
+        })
+    ],
+    listeners: {
+        itemdblclick: function (me, record, item, index, e, eOpts) {
+            //双击事件的操作
+        }
+    },
+    bbar: { xtype: "pagingtoolbar", store: store, displayInfo: true }
+});
 
 Ext.create('Ext.Viewport', {
         items: [
             {
                 layout: 'fit',
                 region: 'center',
-                items: [communityHandleGrid]
+                items: [grid]
             }
         ],
 
     });
+
+});
