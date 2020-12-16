42bf3223a80995afd7c7bc1e7392f9a56baeec76
cooper <enen7373@gmail.com>
2020-12-16 17:28:00 +0800

    finish edit box

--- editBox/editbox.js
+++ editBox/editbox.js
@@ -1,14 +1,35 @@
 Ext.onReady(function(){
 
+
+    var states = Ext.create('Ext.data.Store', {
+        fields: ['abbr', 'name'],
+        data : [
+            {"abbr":"AL", "name":"Alabama"},
+            {"abbr":"AK", "name":"Alaska"},
+            {"abbr":"AZ", "name":"Arizona"}
+        ]
+    });
+
     Ext.create('Ext.Viewport', {
         
-        items: [{
+        items: [
+        {
             xtype: 'textfield',
-            fieldLabel: 'Name',
+            fieldLabel: '编辑框',
             allowBlank: false 
-        }, {
-            xtype: 'textfield',
-            fieldLabel: 'Email Address',
-        }]
+        },
+        {
+            xtype: 'combo',
+            fieldLabel: '下拉框',
+            store: states,
+            displayField: 'name',
+            valueField: 'abbr'
+        },
+        {
+            xtype: 'datefield',
+            fieldLabel: '日期选择框',
+            value: new Date()
+        }
+    ]
     });
 })
